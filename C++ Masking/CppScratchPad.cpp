70aeaec1492ed99bae68fd61c77d3e30a5db4b26
Kulpot <kulpotwahu@gmail.com>
2024-01-16 02:58:32 +0800

    ...

--- C++ Masking/CppScratchPad.cpp
+++ C++ Masking/CppScratchPad.cpp
@@ -2,37 +2,76 @@
 using std::cout;
 using std::endl;
 
+// --------------------- C++ Masking ---------------------------------------------------
+//ref link:https://www.youtube.com/watch?v=vf0n7te3_Vc&list=PLRwVmtr-pp043Ah3GxAqNyoMGyo3_LDNh&index=3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // ------------------------- C++ Bitwise AND and OR ---------------------------------------------------
 //ref link:https://www.youtube.com/watch?v=qTFUnp_KwHg&list=PLRwVmtr-pp043Ah3GxAqNyoMGyo3_LDNh&index=2
 
-void main()
-{
-	// 0000 0000 0000 0001
-	//short myValue = 1;
-	// 0000 0000 0000 0100
-	//short yourValue = 4;
-
-	//bool b = true && false;		// AND
-	//bool b = true || false;		// OR
-
-	//short result = myValue & yourValue;
-	// 0000 0000 0000 0001		1
-	// 0000 0000 0000 0100		4
-	// 0000 0000 0000 0000		0
-	//short result = myValue | yourValue;
-	// 0000 0000 0000 0001		1
-	// 0000 0000 0000 0100		4
-	// 0000 0000 0000 0101		5	
-	short myValue = 3;
-	short yourValue = 6;
-	short result = myValue & yourValue;
-	// 0000 0000 0000 0011		3
-	// 0000 0000 0000 0110		6
-	// 0000 0000 0000 0010		2
-
-	//cout << myValue << endl;
-	cout << result << endl;
-}
+//void main()
+//{
+//	// 0000 0000 0000 0001
+//	//short myValue = 1;
+//	// 0000 0000 0000 0100
+//	//short yourValue = 4;
+//
+//	//bool b = true && false;		// AND
+//	//bool b = true || false;		// OR
+//
+//	//short result = myValue & yourValue;
+//	// 0000 0000 0000 0001		1
+//	// 0000 0000 0000 0100		4
+//	// 0000 0000 0000 0000		0
+//	//short result = myValue | yourValue;
+//	// 0000 0000 0000 0001		1
+//	// 0000 0000 0000 0100		4
+//	// 0000 0000 0000 0101		5	
+//	short myValue = 3;
+//	short yourValue = 6;
+//	short result = myValue & yourValue;
+//	// 0000 0000 0000 0011		3
+//	// 0000 0000 0000 0110		6
+//	// 0000 0000 0000 0010		2
+//
+//	//cout << myValue << endl;
+//	cout << result << endl;
+//}
 
 
 
